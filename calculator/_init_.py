1b1b48e525d9797d3f0cbf64591b6517852d2eaf
Livia-1212 <62072577+Livia-1212@users.noreply.github.com>
2024-09-25 19:42:56 -0400

    modify main branch calculators and tests

--- calculator/_init_.py
+++ calculator/_init_.py
@@ -1,18 +1,5 @@
-class calculator:
-    @staticmethod
-    def add (a,b):
-        calculation = Calculation(a, b, add)
-        return calculation.get_result()
-    @staticmethod 
-    def subtract (a,b):
-        calculation = Calculation(a,b, subtract)
-        return calculation.get_result()
-    @staticmethod 
-    def multiply (a,b):
-        calculation = Calculation(a,b, multiply)
-        return calculation.get_result()
-    @staticmethod 
-    def divide(a,b):
-        calculation = Calculation(a,b, divide)
-        return calculation.get_result()
-    
+def add(a,b):
+    return a + b
+
+def subtract(a,b):
+    return a - b
