0728cff3dd304b14e2ba5682cf145ddbfb038944
Livia-1212 <62072577+Livia-1212@users.noreply.github.com>
2024-09-26 13:13:38 -0400

    modified tests

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,5 +1,10 @@
+import sys
+sys.path.append('/Users/liviali/Documents/NJIT/'+
+                'Fall_2024/WebSystemDev_IS601853/'+
+                'GitHubHomework/Projects3/calculator_3_levels/calculator/_init_.py')
+
 '''My Calculator Test'''
-from calculator import add, subtract
+from calculator._init_ import add, subtract # type: ignore
 
 def test_addition():
     '''Test that addition function works '''    
