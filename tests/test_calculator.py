3a6b095599dc56f35479886840f4c9f020d4c983
Livia-1212 <62072577+Livia-1212@users.noreply.github.com>
2024-09-26 23:02:51 -0400

    updated Part3

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -7,7 +7,6 @@ import sys
 sys.path.append('/Users/liviali/Documents/'+
                 'NJIT/Fall_2024/WebSystemDev_IS601853/'+
                 'GitHubHomework/Projects3/calculator_3_levels')
->>>>>>> f511910b9e5e1e76d6ee3d7cf65d6edfabdd7f48
 
 from calculator.sample import add, subtract
 def test_addition():
@@ -16,7 +15,6 @@ def test_addition():
 
 def test_subtraction():
     '''Test that addition function works '''    
-<<<<<<< HEAD
     assert Calculator.subtract(2,2) == 0
 
 def test_divide():
@@ -26,6 +24,4 @@ def test_divide():
 def test_multiply():
     '''Test that multiply function works '''    
     assert Calculator.multiply(2,2) == 4
-=======
     assert subtract(2,2) == 0
->>>>>>> f511910b9e5e1e76d6ee3d7cf65d6edfabdd7f48
